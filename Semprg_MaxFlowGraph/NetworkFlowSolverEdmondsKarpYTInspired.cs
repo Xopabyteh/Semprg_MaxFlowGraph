f17285e5b8e83e5f813ac93b2cc5e5f6b6b8cba3
Xopabyteh <xopabyteh1337@gmail.com>
2025-03-01 10:33:02 +0100

    😎

---
+++ Semprg_MaxFlowGraph/NetworkFlowSolverEdmondsKarpYTInspired.cs
@@ -0,0 +1,168 @@
+﻿// Heavily inspired by:
+// and "Edmonds Karp Algorithm | Source Code" - https://www.youtube.com/watch?v=OViaWp9Q-Oc
+
+class NetworkFlowSolverEdmondsKarpYTInspired
+{
+    // What an interesting way for keeping track of visited nodes :)
+    // - node_i is visited if visited[i] == visitedToken
+    // - to reset visited nodes, increment visitedToken
+    private int visitedToken = 1;
+    private int[] visited;
+
+    private int maxFlow;
+
+    /// <summary>
+    /// Element at i returns all edges outgoing from that node_i. <br/>
+    /// [from_i] -> list of edges where .From is [from_i]
+    /// </summary>
+    private List<Edge>[] graph;
+
+    private readonly int _source;
+    private readonly int _sink;
+    private readonly int _vertexCount;
+
+    public NetworkFlowSolverEdmondsKarpYTInspired(int vertexCount, int source, int sink, TestSettingEdge[] tEdges)
+    {
+        _vertexCount = vertexCount;
+        _source = source;
+        _sink = sink;
+        visited = new int[vertexCount];
+
+        // Create edges
+        graph = Enumerable.Range(0, vertexCount)
+            .Select(_ => new List<Edge>())
+            .ToArray();
+
+        foreach (var tEdge in tEdges)
+        {
+            var edge = new Edge(tEdge.From, tEdge.To, tEdge.Capacity);
+            var residual = new Edge(tEdge.To, tEdge.From, capacity: 0);
+            
+            edge.Residual = residual;
+            residual.Residual = edge;
+        
+            graph[edge.From].Add(edge);
+            graph[residual.From].Add(residual);
+        }
+    }
+
+    private void MarkNodeVisited(int i)
+        => visited[i] = visitedToken;
+
+    private bool IsNodeVisited(int i)
+        => visited[i] == visitedToken;
+    private void ClearVisited()
+        => visitedToken++;
+
+    public int SolveForMaxFlow()
+    {
+        maxFlow = 0;
+        int bottleNeck;
+
+        // Find augmenting path and augment the flow
+        // until no more augmenting paths are found
+        do
+        {
+            bottleNeck = FindAugmentingPathBFS();
+            maxFlow += bottleNeck;
+            ClearVisited();
+        } while (bottleNeck != 0);
+        
+        return maxFlow;
+    }
+
+
+    /// <summary>
+    /// Find augmenting path using BFS.
+    /// Marks visited nodes and returns the bottleNeck value.
+    /// Augments the edges on the path.
+    /// </summary>
+    private int FindAugmentingPathBFS()
+    {
+        var queue = new Queue<int>(_vertexCount);
+
+        MarkNodeVisited(_source);
+        queue.Enqueue(_source);
+
+        // For rebuilding the path
+        // indexing at node_i returns the edge that led to node_i
+        var prevPath = new Edge[_vertexCount];
+        
+        while (queue.TryDequeue(out var node))
+        {
+            if(node == _sink)
+                break;
+
+            // For each edge outgoing from node
+            foreach (var edge in graph[node])
+            {
+                var capacity = edge.RemainingCapacity();
+                if (capacity > 0 && !IsNodeVisited(edge.To))
+                {
+                    MarkNodeVisited(edge.To);
+                    prevPath[edge.To] = edge;
+                    queue.Enqueue(edge.To);
+                }
+            }
+        }
+
+        // No found edge leading to sink
+        if (prevPath[_sink] == null)
+            return 0;
+
+        // -> Augmenting path exists
+        // Find bottleneck
+        var bottleNeck = int.MaxValue;
+        for (int i = _sink;;)
+        {
+            var prevEdge = prevPath[i];
+            if (prevEdge == null)
+                break;
+
+            bottleNeck = Math.Min(bottleNeck, prevEdge.RemainingCapacity());
+            i = prevEdge.From;
+        }
+
+        // Augment by bottleneck
+        for (int i = _sink;;)
+        {
+            var prevEdge = prevPath[i];
+            if (prevEdge == null)
+                break;
+
+            prevEdge.Augment(bottleNeck);
+            i = prevEdge.From;
+        }
+
+        return bottleNeck;
+    }
+
+    public class Edge
+    {
+        public int From { get; init; }
+        public int To { get; init; }
+        public int Flow { get; private set; }
+        public Edge Residual { get; set; }
+        public int Capacity { get; init; }
+
+        public Edge(int from, int to, int capacity)
+        {
+            From = from;
+            To = to;
+            Capacity = capacity;
+        }
+
+        public bool IsResidual() 
+            => Capacity == 0;
+
+        public int RemainingCapacity()
+            => Capacity - Flow;
+
+        public void Augment(int bottleNeck)
+        {
+            Flow += bottleNeck;
+            Residual.Flow -= bottleNeck;
+        }
+    }
+
+}
---
+++ Semprg_MaxFlowGraph/NetworkFlowSolverMyOldSketchy.cs
@@ -0,0 +1,150 @@
+﻿// Heavily inspired by https://github.com/hakenr/Programiste.CSharp/tree/master/MaxFlow#readme
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// This solver is sketch, because it does not handle residual paths.
+/// But due to nature of the BFS, it seems work anyway
+/// </summary>
+readonly struct NetworkFlowSolverMyOldSketchy
+{
+    public int SolveForMaxFlow(TestSetting setting)
+    {
+        var usedFlows = new Dictionary<TestSettingEdge, int>(setting.EdgeCount);
+        var maxFlow = 0;
+        while (true)
+        {
+            // 1. Find augmentation path (available path from source to sink)
+            var path = FindAugmentationPath(setting.Edges, setting.Source, setting.Sink, usedFlows)!;
+
+            if (path is null)
+            {
+                // No path found
+                break;
+            }
+
+            // 2. Find bottleneck (minimum capacity on the path)
+            Debug.Assert(path.All(e => e.Capacity - usedFlows.GetValueOrDefault(e, 0) > 0), "Zero residual capacity detected in augmenting path!");
+            var minCapacity = path.Min(e =>
+                e.Capacity - usedFlows.GetValueOrDefault(e, 0) // Capacity - UsedFlow
+            );
+
+            // 3. Augment flow (update used flows)
+            foreach (var edge in path)
+            {
+                if (!usedFlows.ContainsKey(edge))
+                {
+                    usedFlows[edge] = 0;
+                }
+
+                usedFlows[edge] += minCapacity;
+            }
+
+            // 4. Update max flow
+            maxFlow += minCapacity;
+
+            // 5. Repeat
+        }
+
+        return maxFlow;
+    }
+
+    TestSettingEdge[]? FindAugmentationPath(
+        TestSettingEdge[] Edges,
+        int source,
+        int sink,
+        Dictionary<TestSettingEdge, int> usedFlows)
+    {
+        // 1. Find path
+        // Bfs search for path (IGNORE FOR NOW: including residual paths)
+        // Look at node
+        // Find its neighbors
+        // Add all which have flow capacity `(Capacity - UsedFlow) > 0` to queue
+        // If we find sink, we are done
+
+        // This will result in finding the shortest augmenting path (in terms of edge count)
+
+        var queue = new Queue<int>();
+        var visited = new HashSet<int>();
+        var shortestPathNodes = new Dictionary<int, int>(); // Key is "to", value is "from"
+
+        queue.Enqueue(source);
+        visited.Add(source);
+
+        while (queue.TryDequeue(out var node))
+        {
+            // `From` contains "node", and `To` contains "neighbor"
+            var nodeToNeighborTestSettingEdges = Edges
+                .Where(e =>
+                    e.From == node  // Find edges from the node
+                    && !visited.Contains(e.To)
+                    && e.Capacity - usedFlows.GetValueOrDefault(e, 0) > 0) // Find edges with remaining capacity
+                .ToArray();
+
+            foreach (var edge in nodeToNeighborTestSettingEdges)
+            {
+                if (shortestPathNodes.ContainsKey(edge.To))
+                    continue;
+
+                Debug.Assert(!visited.Contains(edge.To), "Neighbor already visited");
+                Debug.Assert(edge.Capacity - usedFlows.GetValueOrDefault(edge, 0) > 0, "Zero residual capacity detected in augmenting path!");
+
+                shortestPathNodes[edge.To] = edge.From;
+                visited.Add(edge.To);
+                queue.Enqueue(edge.To);
+
+                if (edge.To == sink)
+                {
+                    // We found the sink
+                    return ReconstructEdgePath(
+                        ReconstructPath(shortestPathNodes, Edges.Length, sink),
+                        Edges
+                    );
+                }
+            }
+        }
+
+        // No path found
+        return null;
+    }
+
+    ReadOnlySpan<int> ReconstructPath(Dictionary<int, int> parent, int edgeCount, int sink)
+    {
+        var reversePath = new List<int>(edgeCount);
+        // Go backwards from sink to source
+        var current = sink;
+        while (true)
+        {
+            //reversePath[pathIndex] = current;
+            reversePath.Add(current);
+
+            var previous = parent.GetValueOrDefault(current, -1);
+            if (previous == -1)
+            {
+                // No path found
+                break;
+            }
+
+            current = previous;
+        }
+
+        var span = CollectionsMarshal.AsSpan(reversePath);
+        span.Reverse();
+        return span;
+    }
+
+    TestSettingEdge[] ReconstructEdgePath(ReadOnlySpan<int> path, TestSettingEdge[] graph)
+    {
+        var edgePath = new TestSettingEdge[path.Length - 1];
+        for (int i = 0; i < path.Length - 1; i++)
+        {
+            var from = path[i];
+            var to = path[i + 1];
+            var edge = graph.First(e => e.From == from && e.To == to);
+            edgePath[i] = edge;
+        }
+
+        return edgePath;
+    }
+}
